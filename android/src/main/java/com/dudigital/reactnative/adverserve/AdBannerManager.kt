41b68a8ab14f9de09f8dd128d42fc626f5c44fc1
Hunaid Hassan <hhunaid@gmail.com>
2019-01-02 02:14:02 +0500

    Builds with RN

---
+++ android/src/main/java/com/dudigital/reactnative/adverserve/AdBannerManager.kt
@@ -0,0 +1,8 @@
+package com.dudigital.reactnative.adverserve
+
+import com.facebook.react.uimanager.SimpleViewManager
+import com.adition.android.sdk.AditionView
+
+class AdBannerManager: SimpleViewManager<AditionView> {
+
+}
